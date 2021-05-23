9fb5c8db21b2d4b1a5874dcb024e89306a6beee1
jifrozen <answldjs1836@naver.com>
2021-05-23 21:18:01 +0900

    [Feat] Create Swagger config

---
+++ member/src/main/java/kr/or/dining_together/member/config/SwaggerConfig.java
@@ -0,0 +1,31 @@
+package kr.or.dining_together.member.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket swaggerApi(){
+        return new Docket(DocumentationType.SWAGGER_2).apiInfo(swaggerInfo()).select()
+                .apis(RequestHandlerSelectors.basePackage("kr.or.dining_together.member.controller"))
+                .paths(PathSelectors.ant("/auth/**"))
+                .build()
+                .useDefaultResponseMessages(false); // 기본으로 세팅되는 200,401,403,404 메시지를 표시 하지 않음
+    }
+
+    private ApiInfo swaggerInfo() {
+        return new ApiInfoBuilder().title("Member API Documentation")
+                .description("회원  API 문서")
+                .version("1").build();
+    }
+}
