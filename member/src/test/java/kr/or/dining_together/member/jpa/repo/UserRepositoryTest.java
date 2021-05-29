c74e2e6a0c4a35b1fd641af8d45700a6ff180244
jifrozen <answldjs1836@naver.com>
2021-05-29 12:59:01 +0900

    [Test] Create UserRepositoryTest

--- member/src/test/java/kr/or/dining_together/member/jpa/repo/UserRepositoryTest.java
+++ member/src/test/java/kr/or/dining_together/member/jpa/repo/UserRepositoryTest.java
@@ -1,46 +1,50 @@
 package kr.or.dining_together.member.jpa.repo;
 
-import java.util.Arrays;
+import static org.junit.Assert.*;
+
+import java.util.Collections;
 import java.util.Date;
-import java.util.List;
+import java.util.Optional;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import kr.or.dining_together.member.jpa.entity.User;
-import kr.or.dining_together.member.vo.LoginRequest;
-import lombok.extern.java.Log;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
-@Log
+@DataJpaTest
 public class UserRepositoryTest {
+
 	@Autowired
-	UserRepository userRepository;
+	private UserRepository userRepository;
+
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@Test
-	public void insertTest() {
-		List<String> roles = Arrays.asList("USER");
-		User user = User.builder()
+	public void findByEmail() {
+		String email = "jifrozen@naver.com";
+		String name = "문지언";
+		//given
+		userRepository.save(User.builder()
 			.id(1L)
-			.email("qja9605@naver.com")
-			.name("신태범")
-			.password("1234")
-			.phoneNo("010-2691-3895")
+			.email(email)
+			.name(name)
+			.password(passwordEncoder.encode("test1111"))
+			.phoneNo("010-1234-5678")
 			.joinDate(new Date())
-			.roles(roles)
-			.build();
-
-		userRepository.save(user);
-
-		LoginRequest loginRequest = new LoginRequest();
-		loginRequest.setEmail("qja9605@naver.com");
-		loginRequest.setPassword("1234");
-
-		System.out.println(user);
+			.roles(Collections.singletonList("ROLE_USER"))
+			.build());
+		//when
+		Optional<User> user = userRepository.findByEmail(email);
+		//then
+		assertNotNull(user);
+		assertTrue(user.isPresent());
+		assertEquals(user.get().getName(), name);
 
 	}
 
