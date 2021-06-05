c44b6e8406c076b310a8c56f4a2e8a9f4bdb17d7
jifrozen <answldjs1836@naver.com>
2021-06-05 13:49:04 +0900

    [Feat] Naver login

--- member/src/main/java/kr/or/dining_together/member/controller/SignController.java
+++ member/src/main/java/kr/or/dining_together/member/controller/SignController.java
@@ -85,7 +85,18 @@ public class SignController {
 		@ApiParam(value = "서비스 제공자 provider", required = true, defaultValue = "kakao") @PathVariable String provider,
 		@ApiParam(value = "소셜 access token", required = true) @RequestParam String accessToken) {
 
-		User signedUser = userService.signupByKakao(accessToken, provider);
+		User signedUser = null;
+
+		switch (provider) {
+			case "naver":
+				signedUser = userService.signupByNaver(accessToken, provider);
+				;
+				break;
+			case "kakao":
+				signedUser = userService.signupByKakao(accessToken, provider);
+				;
+				break;
+		}
 		return responseService.getSingleResult(
 			jwtTokenProvider.createToken(String.valueOf(signedUser.getEmail()), signedUser.getRoles()));
 
