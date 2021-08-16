68548ee47f373b5b417801c2287b0db103aa1120
TaeBeomShin <qja9605@gmail.com>
2021-08-16 17:31:11 +0900

    [Fix] post에서 get으로 변경

--- search/src/main/java/kr/or/dining_together/search/controller/LogServiceController.java
+++ search/src/main/java/kr/or/dining_together/search/controller/LogServiceController.java
@@ -1,5 +1,6 @@
 package kr.or.dining_together.search.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,25 +23,25 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class LogServiceController {
 	@ApiOperation(value = "경매 조회수 로그", notes = "1주일간의 경매 조회수 로그 정보를 반환한다.")
-	@PostMapping(value = "/auctionview")
+	@GetMapping(value = "/auctionview")
 	public String getAuctionViewLogURL(@RequestParam @ApiParam(value = "경매 id", required = true)String auctionId) {
 		String string ="<iframe src=\"http://118.67.133.150:5601/app/dashboards#/view/bc304780-fc61-11eb-b3ad-433340f88220?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:ac62d3f0-fc60-11eb-b3ad-433340f88220,key:someField.actionType.keyword,negate:!f,params:(query:view),type:phrase),query:(match_phrase:(someField.actionType.keyword:view))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:ac62d3f0-fc60-11eb-b3ad-433340f88220,key:someField.auctionId,negate:!f,params:(query:'"+auctionId+"'),type:phrase),query:(match_phrase:(someField.auctionId:'"+auctionId+"')))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:'auction:view',viewMode:view)&hide-filter-bar=true\" height=\"600\" width=\"400\"></iframe>";
 		return string;
 	}
 	@ApiOperation(value = "경매 입찰 조회수 로그", notes = "1주일간의 경매 입찰 로그 정보를 반환한다.")
-	@PostMapping(value = "/auctionbid")
+	@GetMapping(value = "/auctionbid")
 	public String getAuctionBidLogURL(@RequestParam @ApiParam(value = "경매 id", required = true)String auctionId) {
 		String iframeString ="<iframe src=\"http://118.67.133.150:5601/app/dashboards#/view/6c16f310-fc62-11eb-b3ad-433340f88220?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:ac62d3f0-fc60-11eb-b3ad-433340f88220,key:someField.actionType.keyword,negate:!f,params:(query:bid),type:phrase),query:(match_phrase:(someField.actionType.keyword:bid))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:ac62d3f0-fc60-11eb-b3ad-433340f88220,key:someField.auctionId,negate:!f,params:(query:'"+auctionId+"'),type:phrase),query:(match_phrase:(someField.auctionId:'"+auctionId+"')))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:'auction:bid',viewMode:view)&hide-filter-bar=true\" height=\"600\" width=\"400\"></iframe>";
 		return iframeString;
 	}
 	@ApiOperation(value = "가게 조회수 로그", notes = "1주일간의 가게 조회수 로그 정보를 반환한다.")
-	@PostMapping(value = "/storeview")
+	@GetMapping(value = "/storeview")
 	public String getStoreViewLogURL(@RequestParam @ApiParam(value = "가게 id", required = true)String storeId) {
 		String iframeString="<iframe src=\"http://118.67.133.150:5601/app/dashboards#/view/7c7144b0-fc60-11eb-b3ad-433340f88220?embed=true&_g=(filters:!(),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(description:'',filters:!(('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'2cb520e0-fc60-11eb-b3ad-433340f88220',key:someField.storeId,negate:!f,params:(query:'"+storeId+"'),type:phrase),query:(match_phrase:(someField.storeId:'"+storeId+"'))),('$state':(store:appState),meta:(alias:!n,disabled:!f,index:'2cb520e0-fc60-11eb-b3ad-433340f88220',key:someField.actionType.keyword,negate:!f,params:(query:view),type:phrase),query:(match_phrase:(someField.actionType.keyword:view)))),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:'store:view',viewMode:view)&hide-filter-bar=true\" height=\"600\" width=\"400\"></iframe>";
 		return iframeString;
 	}
 	@ApiOperation(value = "가게 리뷰 로그", notes = "1주일간의 가게 리뷰 로그 정보를 반환한다.")
-	@PostMapping(value = "/storereview")
+	@GetMapping(value = "/storereview")
 	public String getStoreReviewLogURL(@RequestParam @ApiParam(value = "가게 id", required = true)String storeId) {
 		String iframeString ="<iframe src=\"http://118.67.133.150:5601/app/dashboards#/view/82fbd630-fdc1-11eb-b3ad-433340f88220?embed=true&_g=(filters:!(('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'0a7901b0-fdc1-11eb-b3ad-433340f88220',key:someField.actionType.keyword,negate:!f,params:(query:review),type:phrase),query:(match_phrase:(someField.actionType.keyword:review))),('$state':(store:globalState),meta:(alias:!n,disabled:!f,index:'0a7901b0-fdc1-11eb-b3ad-433340f88220',key:someField.storeId,negate:!f,params:(query:'"+storeId+"'),type:phrase),query:(match_phrase:(someField.storeId:'"+storeId+"')))),refreshInterval:(pause:!t,value:0),time:(from:now-7d,to:now))&_a=(description:'',filters:!(),fullScreenMode:!f,options:(hidePanelTitles:!f,useMargins:!t),query:(language:kuery,query:''),timeRestore:!f,title:'store:review',viewMode:view)&hide-filter-bar=true\" height=\"600\" width=\"400\"></iframe>";
 		return iframeString;
