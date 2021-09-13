8533f09d8c4303201c5afb9fc398e9d5614fff8f
TaeBeomShin <qja9605@gmail.com>
2021-09-13 20:54:43 +0900

    [Fix] ncp 서버 변경

--- search/src/main/java/kr/or/dining_together/search/controller/AuctionSearchController.java
+++ search/src/main/java/kr/or/dining_together/search/controller/AuctionSearchController.java
@@ -2,6 +2,7 @@ package kr.or.dining_together.search.controller;
 
 import java.util.List;
 
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
