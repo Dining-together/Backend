09e2b43a781597cbdcb1c9e005776f54bdd0097a
jifrozen <answldjs1836@naver.com>
2021-10-02 19:12:48 +0900

    [Feat] Exception 처리

--- chat/src/main/java/kr/or/dining_together/chat/ChatApplication.java
+++ chat/src/main/java/kr/or/dining_together/chat/ChatApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+
+import kr.or.dining_together.chat.advice.exception.FeignErrorDecoder;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,6 +15,10 @@ public class ChatApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChatApplication.class, args);
+	}
+	@Bean
+	public FeignErrorDecoder getFeignErrorDecoder() {
+		return new FeignErrorDecoder();
 	}
 
 }
---
+++ chat/src/main/java/kr/or/dining_together/chat/advice/ExceptionAdvice.java
@@ -0,0 +1,80 @@
+package kr.or.dining_together.chat.advice;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import com.google.gson.JsonObject;
+
+import kr.or.dining_together.chat.advice.exception.BadRequestException;
+import kr.or.dining_together.chat.advice.exception.ResourceNotExistException;
+import kr.or.dining_together.chat.advice.exception.UserNotFoundException;
+import kr.or.dining_together.chat.model.CommonResult;
+import kr.or.dining_together.chat.service.ResponseService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @package : kr.or.dining_together.auction.advice
+ * @name: ExceptionAdvice.java
+ * @date : 2021/06/05 9:59 오후
+ * @author : jifrozen
+ * @version : 1.0.0
+ * @description : 결과 처리
+ * @modified :
+ **/
+@RestControllerAdvice
+@RequiredArgsConstructor
+@Slf4j
+public class ExceptionAdvice {
+
+	private final ResponseService responseService;
+
+	@ExceptionHandler(Exception.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+		exceptionLogCall(e.toString());
+
+		return responseService.getFailResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), "실패");
+	}
+
+	@ExceptionHandler(ResourceNotExistException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	protected CommonResult resourceNotExistException(HttpServletRequest request, ResourceNotExistException e) {
+		exceptionLogCall(e.toString());
+
+		return responseService.getFailResult(HttpStatus.NOT_FOUND.value(), "요청한 자원이 존재 하지 않습니다.");
+	}
+
+	@ExceptionHandler(UserNotFoundException.class)
+	@ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+	protected CommonResult userNotFoundException(HttpServletRequest request, UserNotFoundException e) {
+		exceptionLogCall(e.toString());
+
+		return responseService.getFailResult(HttpStatus.INTERNAL_SERVER_ERROR.value(), "사용자가 존재하지 않습니다.");
+	}
+
+	@ExceptionHandler(BadRequestException.class)
+	@ResponseStatus(HttpStatus.BAD_REQUEST)
+	protected CommonResult badRequestException(HttpServletRequest request, BadRequestException e) {
+		exceptionLogCall(e.toString());
+
+		return responseService.getFailResult(HttpStatus.BAD_REQUEST.value(), "요청 자원이 알맞지 않습니다..");
+	}
+
+	public void exceptionLogCall(String errorMessage){
+		JsonObject jsonObject = new JsonObject();
+		jsonObject.addProperty("msgType","tracking");
+		jsonObject.addProperty("logType","info");
+		jsonObject.addProperty("target", LocalDateTime.now().format(DateTimeFormatter.ofPattern("yyyy-MM-dd")));
+		jsonObject.addProperty("content",errorMessage);
+
+		log.info(String.valueOf(jsonObject));
+	}
+}
