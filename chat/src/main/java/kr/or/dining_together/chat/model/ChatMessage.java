888047632108b3e61d48a030897bc63a16581e4f
jifrozen <answldjs1836@naver.com>
2021-10-07 01:40:34 +0900

    [Fix] json 형식

--- chat/src/main/java/kr/or/dining_together/chat/model/ChatMessage.java
+++ chat/src/main/java/kr/or/dining_together/chat/model/ChatMessage.java
@@ -9,6 +9,8 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -29,6 +31,7 @@ public class ChatMessage {
 	private String sender; // 메시지 보낸사람
 	private String message; // 메시지
 	@ManyToOne(fetch = FetchType.LAZY)
+	@JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
 	@JoinColumn(name = "chatroom_id")
 	private ChatRoom chatRoom;
 
