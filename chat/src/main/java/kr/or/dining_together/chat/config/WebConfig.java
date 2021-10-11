899be6b46ce1e190f93f9936ce7dd03073e686f0
jifrozen <answldjs1836@naver.com>
2021-10-11 14:41:04 +0900

    [Fix] CORS config

--- chat/src/main/java/kr/or/dining_together/chat/config/WebConfig.java
+++ chat/src/main/java/kr/or/dining_together/chat/config/WebConfig.java
@@ -1,14 +1,29 @@
 package kr.or.dining_together.chat.config;
 
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import kr.or.dining_together.chat.config.Filter.CORSFilter;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+	@Bean
+	public FilterRegistrationBean getFilterRegistrationBean(){
+		FilterRegistrationBean registrationBean = new FilterRegistrationBean(new CORSFilter());
+		registrationBean.addUrlPatterns("/**");
+		return registrationBean;
+	}
+
+	// CORS 추가
 	@Override
 	public void addCorsMappings(CorsRegistry registry) {
-		registry.addMapping("/**").allowedMethods("*").allowedOriginPatterns("*");
+		registry.addMapping("/**")
+			.allowedOrigins("*://*")
+			.allowedHeaders("*")
+			.allowedMethods("*");
 	}
 }
